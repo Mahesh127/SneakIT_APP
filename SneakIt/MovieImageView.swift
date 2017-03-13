4bac8556c9c057394d2838a8533af79e5af0a94f
Mahesh Bikumalla <mbikumalla@mbikumalla-ltm.internal.salesforce.com>
2017-03-13 10:39:56 -0400

    First Commit

---
+++ SneakIt/MovieImageView.swift
@@ -0,0 +1,21 @@
+//
+//  MovieImageView.swift
+//  SneakIt
+//
+//  Created by Mahesh Bikumalla on 3/9/17.
+//  Copyright © 2017 Mahesh Bikumalla. All rights reserved.
+//
+
+import UIKit
+
+class MovieImageView: UIImageView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
